151d5e50afaa20b73d9f7f6ddc1142598d25c96b
itsmebroarif.dev <68406328+itsmebroarif@users.noreply.github.com>
2023-06-07 23:48:03 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* Bagian ini boleh dihapus ketika kalian tidak menggunakannya */
+const nama = prompt("Silahkan masukan nama lengkap mu disini! ");
+document.write(`<h1>
+    Selamat Datang ${nama}!
+</h1>`);
+document.write(`<p>
+    ini adalah tempat kalian mengerjakan beberapa tugas yang ada didalam sesi pembelajaran kita
+</p>`);
